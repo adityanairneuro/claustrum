c2057e2838c3f48eeecc596dfae501bacc939e3e
Aditya Nair <aditya.nair@u.nus.edu>
2020-06-01 00:12:52 +0800

    Add files via upload

---
+++ Required_Packages.R
@@ -0,0 +1,17 @@
+## Packages required for claustrum classifier
+
+# Please run this script to install required packages before running the software
+
+install.packages("shiny")
+install.packages("pracma")
+install.packages("DT")
+install.packages("shinythemes")
+install.packages("neuralnet")
+install.packages("nnet")
+install.packages("httr")
+install.packages("plotly")
+install.packages("radarchart")
+install.packages("rhandsontable")
+install.packages("lime")
+install.packages("ggplot2")
+install.packages("caret")
